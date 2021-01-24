922a444fa882a9fdd7837f79a97a2f811039f61c
benjamindimant <benjamin@dimant.com>
2021-01-24 01:29:01 -0600

    Make API work

--- backend/flaskAPI.py
+++ backend/flaskAPI.py
@@ -1,33 +1,19 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from text_similarity import categorise
-from werkzeug import secure_filename
-from shutil import copyfile
 import tempfile
 import os
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-# @app.route("/file/<string:filename>/", methods=["GET"])
-# def get_file_from_firebase(filename):
-#     file = Firebase(filename)
-#     return file
-
-
-# @app.route("/parse_file", methods=["GET"])
-# def parse_file(filename):
-#     text = FileParser(filename)
-#     return text
-
-
-@app.route("/machine_learning", methods=["GET"])
+@app.route("/machine_learning", methods=["POST"])
+@cross_origin()
 def machine_learning():
-    f = request.files['file'].read()
-    new_file, filename = tempfile.mkstemp()
-    os.write(new_file, f)
+    f = request.files['file']
+    f.save('/tmp/' + f.filename)
     # # this is a list, [0] is major category, [1:] are top three minor matches
-    categories = categorise(filename)
-    os.close(new_file)
+    categories = categorise('/tmp/' + f.filename)
     return jsonify({"major": categories[0], "minor": [categories[1:]]})
 
 
